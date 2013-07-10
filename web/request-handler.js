5740cf6fded17e3c1bfbf1bf149f5127ab840608
Hack Reactor Students <github@hackreactor.com>
2013-07-10 11:47:14 -0700

    Preliminary work on server for spec tests.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,5 +1,22 @@
 exports.datadir = __dirname + "data/sites.txt"; // tests will need to override this.
+var fs = require('fs');
+var http_get = require("http-get");
+var http = require("http");
+var url = require("url");
 
 exports.handleRequest = function (req, res) {
   console.log(exports.datadir);
+  var path = url.parse(req.url).path.split('/');
+  switch (req.method) {
+    case ('POST'):
+      res.writeHead(302);
+      fs.writeFileSync(exports.datadir, path[0]);
+      res.end();
+      break;
+    case('GET'):
+      console.log(req.url);
+      res.writeHead(200);
+      res.end(JSON.stringify(req.data));
+      break;
+  }
 };
